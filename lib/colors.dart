a2021c8849fc63839046a334f09b72f4d72411eb
Matyáš Levíček <matyas.levicek@levicek.net>
2022-05-21 18:55:56 +0200

    Changed the background color

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const pokerSurfaceGreen = Color(0xFF009700);
+const pokerSurfaceGreen = Color(0xFF007000);
 const pokerPrimary = Color(0xFFE3170A);
 const pokerSecondary = Color(0xFF1D4E89);
 const pokerSurfaceWhite = Color(0xFFEFEFEF);
