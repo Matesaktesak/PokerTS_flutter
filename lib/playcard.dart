1b6e9144d2320995223f1ab766955332e681c5ed
Matyáš Levíček <matyas.levicek@levicek.net>
2022-05-21 18:54:53 +0200

    Added card display functionality

--- lib/playcard.dart
+++ lib/playcard.dart
@@ -1,18 +1,76 @@
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class PlayCard  extends StatelessWidget{
-  final int value;
-  final String suit;
+  late int value;
+  late String suit;
+  late String shortname;
+  late String valName;
 
-  const PlayCard(this.value, this.suit, {Key? key}) : super(key: key);
+  PlayCard(this.value, this.suit, {Key? key}) : super(key: key){
+    valName = getNameByValue();
+    shortname = valName.substring(0, valName == "10" ? 2 : 1);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Center(
-        child: Text(value.toString()),
+    Column text = Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Text(
+          shortname,
+          style: Theme.of(context).primaryTextTheme.displaySmall?.copyWith(color: Color(0xFF000000)),
+          
+        ),
+        SizedBox(height: 5,),
+        Image(fit: BoxFit.contain, image: AssetImage("assets/$suit.png")),
+      ]
+    );
+
+    return AspectRatio(
+      aspectRatio: 3/4,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 5),
+          child: Row(
+            children: [
+              Flexible(
+                flex: 2,
+                child: Align(
+                  alignment: Alignment.topLeft,
+                  child: text,
+                ),
+              ),
+              Flexible(
+                flex: 3,
+                child: Container(),
+              ),
+              Flexible(
+                flex: 2,
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: Transform.rotate(angle: math.pi, child: text),
+                ),
+              ),
+            ]
+          ),
+        ),
       ),
     );
   }
 
+
+  String getNameByValue() {
+    // Resolve the integer card value to a non-žulíky-player human readable format /*(as a static method for general use) - legacy*/
+    String out = "";
+    int val = value;
+
+    if (val >= 2 && val <= 10) out = val.toString();
+    if (val == 1 || val == 14) out = "Ace";
+    if (val == 11) out = "Jack";
+    if (val == 12) out = "Queen";
+    if (val == 13) out = "King";
+
+    return out;
+  }
 }
--- lib/playfield.dart
+++ lib/playfield.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
-//import './playcard.dart';
+import 'package:pokerts_app/playcard.dart';
+import 'package:pokerts_app/colors.dart';
 
 class PlayField extends StatefulWidget{
-  //List<PlayCard> cards = [];
-  //List<PlayCard> community = [];
+  List<PlayCard> cards = [];
+  List<PlayCard> community = [
+    PlayCard(10, "Clubs"),
+    PlayCard(5, "Hearts"),
+    PlayCard(12, "Spades"),
+    PlayCard(1, "Diamonds"),
+    PlayCard(9, "Hearts"),
+  ];
   String gameId = "";
 
   PlayField(this.gameId, {Key? key}) : super(key: key);
@@ -26,38 +33,50 @@ class _PlayFieldState extends State<PlayField>{
   Widget build(BuildContext context){
     return Material(
       child: Container(
+        color: pokerSurfaceGreen,
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           verticalDirection: VerticalDirection.down,
           children: [
-            const Expanded(child: Text("Playfield")
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: AspectRatio(
+                aspectRatio: 3*5/4,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: widget.community,
+                ),
+              ),
             ),
-            SizedBox(
-              width: 200,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.baseline,
-                textBaseline: TextBaseline.alphabetic,
-                children: [
-                  Expanded(child: TextField(
-                    autofocus: true,
-                    decoration: const InputDecoration(
-                      labelText: "Bet ammount",
-                      suffixText: "\$",
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: 200,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    Expanded(child: TextField(
+                      autofocus: true,
+                      decoration: const InputDecoration(
+                        labelText: "Bet ammount",
+                        suffixText: "\$",
+                      ),
+                      controller: betFieldController,
+                    )),
+                    SizedBox(width: 12,),
+                    ElevatedButton(
+                      onPressed: (() {
+                        debugPrint("User bet: ${betFieldController.text}");
+                        betFieldController.text = "";
+                      }),
+                      child: const Text("Bet")
                     ),
-                    controller: betFieldController,
-                  )),
-                  SizedBox(width: 12,),
-                  ElevatedButton(
-                    onPressed: (() {
-                      debugPrint("User bet: ${betFieldController.text}");
-                      betFieldController.text = "";
-                    }),
-                    child: const Text("Bet")
-                  ),
-                ],),
+                  ],),
+              ),
             ),
           ],
         ),
